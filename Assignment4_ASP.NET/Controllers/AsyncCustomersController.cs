95fd485a5563f218f70ca376ca77fb04216a0d29
Ankita Khurana <ankita0096@gmail.com>
2018-09-02 14:54:03 +0530

    handle concurrency conflicts in Entity Framework

--- Assignment4_ASP.NET/Controllers/AsyncCustomersController.cs
+++ Assignment4_ASP.NET/Controllers/AsyncCustomersController.cs
@@ -80,6 +80,87 @@ namespace Assignment4_ASP.NET.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int? id, byte[] rowVersion)
+        {
+            string[] fieldsToBind = new string[] { "ID","Name", "DateOfBirth", "RowVersion", "Amount" };
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var  customerToUpdate = await db.Customers.FindAsync(id);
+            if (customerToUpdate == null)
+            {
+                Customer deletedCustomer = new Customer();
+                TryUpdateModel(deletedCustomer, fieldsToBind);
+                ModelState.AddModelError(string.Empty,
+                    "Unable to save changes. The customer was deleted by another user.");
+                ViewBag.InstructorID = new SelectList(db.Customers, "ID", "FullName", deletedCustomer.ID);
+                return View(deletedCustomer);
+            }
+
+
+
+            if (TryUpdateModel(customerToUpdate, fieldsToBind))
+            {
+                try
+                {
+                    db.Entry(customerToUpdate).OriginalValues["RowVersion"] = rowVersion;
+                    await db.SaveChangesAsync();
+
+                    return RedirectToAction("Index");
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    var entry = ex.Entries.Single();
+                    var clientValues = (Customer)entry.Entity;
+                    var databaseEntry = entry.GetDatabaseValues();
+                    if (databaseEntry == null)
+                    {
+                        ModelState.AddModelError(string.Empty,
+                            "Unable to save changes. The customer was deleted by another user.");
+                    }
+                    else
+                    {
+                        var databaseValues = (Customer)databaseEntry.ToObject();
+
+                        if (databaseValues.Name != clientValues.Name)
+                            ModelState.AddModelError("Name", "Current value: "
+                                + databaseValues.Name);
+
+                        if (databaseValues.Amount != clientValues.Amount)
+                            ModelState.AddModelError("Amount", "Current value: "
+                                + databaseValues.Amount);
+
+                        if (databaseValues.DateOfBirth != clientValues.DateOfBirth)
+                            ModelState.AddModelError("DateOfBirth", "Current value: "
+                                + databaseValues.DateOfBirth);                     
+
+                        if (databaseValues.ID != clientValues.ID)
+                            ModelState.AddModelError("ID", "Current value: "
+                        + db.Customers.Find(databaseValues.ID).Name);
+
+                        ModelState.AddModelError(string.Empty, "The record you attempted to edit "
+                            + "was modified by another user after you got the original value. The "
+                            + "edit operation was canceled and the current values in the database "
+                            + "have been displayed. If you still want to edit this record, click "
+                            + "the Save button again. Otherwise click the Back to List hyperlink.");
+                        customerToUpdate.RowVersion = databaseValues.RowVersion;
+                    }
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            ViewBag.InstructorID = new SelectList(db.Customers, "ID", "Name", customerToUpdate.ID);
+            return View(customerToUpdate);
+
+        }
+
+
+        /*
         public async Task<ActionResult> Edit([Bind(Include = "ID,Name,DateOfBirth,RowVersion,Amount")] Customer customer)
         {
             if (ModelState.IsValid)
@@ -90,6 +171,7 @@ namespace Assignment4_ASP.NET.Controllers
             }
             return View(customer);
         }
+        */
 
         // GET: AsyncCustomers/Delete/5
         public async Task<ActionResult> Delete(int? id, bool? concurrencyError)
@@ -103,15 +185,15 @@ namespace Assignment4_ASP.NET.Controllers
             {
                 if (concurrencyError.GetValueOrDefault())
                 {
-                    ViewBag.ConcurrencyErrorMessage = "The record you attempted to delete "
-                    + "was modified by another user after you got the original values. "
-                    + "The delete operation was canceled and the current values in the "
-                    + "database have been displayed. If you still want to delete this "
-                    + "record, click the Delete button again. Otherwise "
-                    + "click the Back to List hyperlink.";
-                   // return RedirectToAction("Index");
+                    //ViewBag.ConcurrencyErrorMessage = "The record you attempted to delete "
+                    //+ "was modified by another user after you got the original values. "
+                    //+ "The delete operation was canceled and the current values in the "
+                    //+ "database have been displayed. If you still want to delete this "
+                    //+ "record, click the Delete button again. Otherwise "
+                    //+ "click the Back to List hyperlink.";
+                   return RedirectToAction("Index");
                 }
-                else
+                //else
                 return HttpNotFound();
             }
             if (concurrencyError.GetValueOrDefault())
